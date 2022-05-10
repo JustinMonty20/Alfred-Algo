d553176ef81273d644245e9bb577caaeb01e52c3
JustinMonty20 <montgomerycodepro@gmail.com>
2022-05-09 21:27:18 -0400

    adding es-lint & token variable to project

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
 /**
  * TODO: Set up implementation with discord.js for AlgoBot
  */
+import {Client, Intents} from "discord.js";
+import {config} from "dotenv";
+config();
+
+const token = process.env.BOT_TOKEN;
+
+console.log("test");
+
